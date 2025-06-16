929e31e381738f5135c8c4c8c666c9b717a3cf18
hsrv <sifirib01@gmail.com>
2025-06-17 01:23:28 +0300

    This plugin automatically converts single line breaks in Markdown text into hard line breaks in the HTML output. This plugin is not enabled by default as this doesn’t follow the semantics of actual Markdown but you may enable it if you’d like parity with Obsidian.

--- quartz.config.ts
+++ quartz.config.ts
@@ -67,6 +67,7 @@ const config: QuartzConfig = {
         },
         keepBackground: false,
       }),
+      Plugin.HardLineBreaks(),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
